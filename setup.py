f150f5ef9e8595eb2ee661a7ae270a9c6ab92379
T0aD <julien.perez@epsylonia.net>
2012-04-27 19:51:22 +0200

    - added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name =              'Azel',
+    version =           '2',
+    py_modules =        ['azel'],
+    url =               'https://github.com/T0aD/azel',
+    author =            'T0aD',
+    description =       'Azel is a python library equivalent to the Elza Perl macro-language (HTTP scripting)'
+    )
